59ee19ea9f67b232dc22c4a535f2a96034b35901
新城 <12399806+createwj@user.noreply.gitee.com>
2023-10-30 15:29:54 +0800

    feat: 1584
    Signed-off-by: 新城 <12399806+createwj@user.noreply.gitee.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
+    mode: 'standalone',
+  }),
+  server: { port: 8080, host: true },
 });
